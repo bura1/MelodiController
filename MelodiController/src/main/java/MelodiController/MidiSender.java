ebb8604ec64a41919db3784970115515208e8aba
bura1 <burictomislavog@gmail.com>
2021-02-23 01:53:15 +0100

    dodane sekunde, ali ne radi

--- MelodiController/src/main/java/MelodiController/MidiSender.java
+++ MelodiController/src/main/java/MelodiController/MidiSender.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.Instant;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sound.midi.InvalidMidiDataException;
@@ -44,25 +46,20 @@ public class MidiSender implements Runnable {
     public void run() {
         
         try {
-            //if (serverSocket.getLocalPort() != 6000) {
-                serverSocket = new ServerSocket(6000);
-            //}
-        } catch (IOException ex) {
-            Logger.getLogger(MidiSender.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        try {
             device = MidiSystem.getMidiDevice(infos[5]);
+            System.out.println(infos.toString());
         } catch (MidiUnavailableException e) {}
 
         try {
             device.open();
         } catch (MidiUnavailableException e) {}
-            
-        if (device.isOpen()) {
-            System.out.println("open");
-        }
 
+        try {
+            serverSocket = new ServerSocket(6000);
+        } catch (IOException ex) {
+            Logger.getLogger(MidiSender.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 socket = serverSocket.accept();
@@ -73,35 +70,44 @@ public class MidiSender implements Runnable {
                 e.printStackTrace();
             }
 
-            if (!Thread.currentThread().isInterrupted()) {
-                ShortMessage midiMsg = new ShortMessage();
-                try {
-                    int note = Integer.parseInt(message.substring(1,3));
+            ShortMessage midiMsg = new ShortMessage();
+            try {
+                int note = Integer.parseInt(message.substring(1,3));
+                
+                long javaTime = Instant.now().toEpochMilli() / 1000;
+                int androidTime = Integer.parseInt(message.substring(3,13));
+                if (javaTime - androidTime < 3) {
+                    System.out.println(javaTime-androidTime);
+                    System.out.println(note);
                     if (message.substring(0,1).equals("1")) {
-                        midiMsg.setMessage(ShortMessage.NOTE_ON, 0, note, 93);
+                        midiMsg.setMessage(ShortMessage.NOTE_ON, 0, 60, 93);
                     }
                     if (message.substring(0,1).equals("0")) {
-                        midiMsg.setMessage(ShortMessage.NOTE_OFF, 1, note, 93);
+                        midiMsg.setMessage(ShortMessage.NOTE_OFF, 1, 60, 93);
                     }
-                } catch (InvalidMidiDataException ex) {
-                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                long timeStamp = -1;
-                Receiver rcvr = null;
-                try {
-                    rcvr = device.getReceiver();
-                } catch (MidiUnavailableException ex) {
-                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                rcvr.send(midiMsg, timeStamp);
+                
+            } catch (InvalidMidiDataException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
+            long timeStamp = -1;
+            Receiver rcvr = null;
+            try {
+                rcvr = device.getReceiver();
+            } catch (MidiUnavailableException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            rcvr.send(midiMsg, timeStamp);            
         }
         
-        // Close
+        /*try {
+            serverSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(MidiSender.class.getName()).log(Level.SEVERE, null, ex);
+        }*/
+        
         device.close();
-        if (!(device.isOpen())) {
-            System.out.println("Closed");
-        }
+        
     }
     
 }
